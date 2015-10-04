e34105facecbe4ea1c4f3636af4e7634d7290810
nashika <info@nashika.net>
2015-10-04 17:56:07 +0900

    pong function

--- src/BlynkBridgeWatcher.cpp
+++ src/BlynkBridgeWatcher.cpp
@@ -1,25 +1,24 @@
 #include <Arduino.h>
 #include "BlynkBridgeWatcher.h"
 
-BlynkBridgeWatcher::BlynkBridgeWatcher(int vPin, const char* name) {
-  _bridge = new WidgetBridge(vPin);
+BlynkBridgeWatcher::BlynkBridgeWatcher(const char* name) {
   _name = name;
 }
 
-BlynkBridgeWatcher::setAuthToken(const char* token) {
-  while (Blynk.connect() == false) {}
-  _bridge.setAuthToken(BRIDGE_WATHER_TOKEN);
+char* BlynkBridgeWatcher::pong() {
+  char* msg = new char[MAX_LENGTH];
+  snprintf(msg, MAX_LENGTH, "%s,$pong", _name);
+  return msg;
 }
 
-BlynkBridgeWatcher::send(const char* command, int argument) {
-  char *msg = "";
-  sprintf(msg, "%d", argument);
-  send(command, msg);
+char* BlynkBridgeWatcher::message(const char* command, int argument) {
+  char* msg = new char[MAX_LENGTH];
+  snprintf(msg, MAX_LENGTH, "%d", argument);
+  message(command, msg);
 }
 
-
-BlynkBridgeWatcher::send(const char* command, const char* argument) {
-  char *msg = "";
-  sprintf(msg, "%d,%d,%d", _name, command, argument);
-  _bridge.virtualWrite(0, msg);
+char* BlynkBridgeWatcher::message(const char* command, const char* argument) {
+  char* msg = new char[MAX_LENGTH];
+  snprintf(msg, MAX_LENGTH, "%s,%s,%s", _name, command, argument);
+  return msg;
 }
--- src/BlynkBridgeWatcher.h
+++ src/BlynkBridgeWatcher.h
@@ -1,17 +1,14 @@
 #ifndef BlynkBridgeWatcher_h
 #define BlynkBridgeWatcher_h
 
-#include <SoftwareSerial.h>
-#include <BlynkSimpleSerial.h>
-
 class BlynkBridgeWatcher {
 public:
-  BlynkBridgeWatcher(int vPin, const char* name);
-  void setAuthToken(const char* token);
-  void send(const char* command, int argument);
-  void send(const char* command, const char* argument);
+  BlynkBridgeWatcher(const char* name);
+  char* pong();
+  char* message(const char* command, int argument);
+  char* message(const char* command, const char* argument);
 private:
-  WidgetBridge _bridge;
+  static const int MAX_LENGTH = 32;
   const char* _name;
 };
 
