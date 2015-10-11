49da283b7cb6914c257e5a66c7ab4083a0b619c0
nashika <info@nashika.net>
2015-10-11 12:26:05 +0900

    implement recv method

--- src/BlynkBridgeWatcher.cpp
+++ src/BlynkBridgeWatcher.cpp
@@ -10,10 +10,34 @@ void BlynkBridgeWatcher::connect(const char* token) {
   _bridge.setAuthToken(token);
 }
 
-void BlynkBridgeWatcher::recv(const char* command) {
-  while (Blynk.connect() == false) {}
+void BlynkBridgeWatcher::recv(const char* input) {
+  char* buff = (char*)input;
   char* msg = new char[MAX_LENGTH];
-  snprintf(msg, MAX_LENGTH, "%s,$pong", _name);
+  char* command, *param1, *param2;
+  command = strtok(buff, ",");
+  param1 = strtok(NULL, ",");
+  param2 = strtok(NULL, ",");
+  if (strcmp(command, "pi") == 0) {
+    snprintf(msg, MAX_LENGTH, "%s,$pong", _name);
+  } else if (strcmp(command, "dr") == 0) {
+    snprintf(msg, MAX_LENGTH, "%s,$read,%d,%d", _name, digitalRead(atoi(param1)), param2);
+  } else if (strcmp(command, "ar") == 0) {
+    snprintf(msg, MAX_LENGTH, "%s,$read,%d,%d", _name, analogRead(atoi(param1)), param2);
+  } else if (strcmp(command, "vr") == 0) {
+    //snprintf(msg, MAX_LENGTH, "%s,$read,%d,%d", _name, Blynk.virtualRead(atoi(param1)), param2);
+    snprintf(msg, MAX_LENGTH, "%s,$undef", _name);
+  } else if (strcmp(command, "dw") == 0) {
+    digitalWrite(atoi(param1), atoi(param2));
+    snprintf(msg, MAX_LENGTH, "%s,$ok", _name);
+  } else if (strcmp(command, "aw") == 0) {
+    analogWrite(atoi(param1), atoi(param2));
+    snprintf(msg, MAX_LENGTH, "%s,$ok", _name);
+  } else if (strcmp(command, "vw") == 0) {
+    Blynk.virtualWrite(atoi(param1), atoi(param2));
+    snprintf(msg, MAX_LENGTH, "%s,$ok", _name);
+  } else {
+    snprintf(msg, MAX_LENGTH, "%s,$undef", _name);
+  }
   _bridge.virtualWrite(0, msg);
 }
 
--- src/BlynkBridgeWatcher.h
+++ src/BlynkBridgeWatcher.h
@@ -1,13 +1,16 @@
 #ifndef BlynkBridgeWatcher_h
 #define BlynkBridgeWatcher_h
 
+#include <string.h>
+#include <stdlib.h>
+
 #include "BlynkBridgeWatcher_BlynkSimpleSerial.h"
 
 class BlynkBridgeWatcher {
 public:
   BlynkBridgeWatcher(int vPin, const char* name);
   void connect(const char* token);
-  void recv(const char* command);
+  void recv(const char* input);
   void send(const char* command, int argument);
   void send(const char* command, const char* argument);
 private:
