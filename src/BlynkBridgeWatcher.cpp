b4c193a28dd12fc7d26dd8ec8d5ad4d28a3d19fd
nashika <info@nashika.net>
2015-10-12 17:29:12 +0900

    read action runnable

--- src/BlynkBridgeWatcher.cpp
+++ src/BlynkBridgeWatcher.cpp
@@ -13,30 +13,31 @@ void BlynkBridgeWatcher::connect(const char* token) {
 void BlynkBridgeWatcher::recv(const char* input) {
   char* buff = (char*)input;
   char* msg = new char[MAX_LENGTH];
-  char* command, *param1, *param2;
+  char* command, *param1, *param2, *param3;
   command = strtok(buff, ",");
   param1 = strtok(NULL, ",");
   param2 = strtok(NULL, ",");
+  param3 = strtok(NULL, ",");
   if (strcmp(command, "pi") == 0) {
-    snprintf(msg, MAX_LENGTH, "%s,$pong", _name);
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s", _name, param2);
   } else if (strcmp(command, "dr") == 0) {
-    snprintf(msg, MAX_LENGTH, "%s,$read,%d,%d", _name, digitalRead(atoi(param1)), param2);
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s,%d", _name, param2, digitalRead(atoi(param1)));
   } else if (strcmp(command, "ar") == 0) {
-    snprintf(msg, MAX_LENGTH, "%s,$read,%d,%d", _name, analogRead(atoi(param1)), param2);
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s,%d", _name, param2, analogRead(atoi(param1)));
   } else if (strcmp(command, "vr") == 0) {
-    //snprintf(msg, MAX_LENGTH, "%s,$read,%d,%d", _name, Blynk.virtualRead(atoi(param1)), param2);
-    snprintf(msg, MAX_LENGTH, "%s,$undef", _name);
+    //snprintf(msg, MAX_LENGTH, "%s,$r,%s,%d", _name, param2, Blynk.virtualRead(atoi(param1)));
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s", _name, param2);
   } else if (strcmp(command, "dw") == 0) {
-    digitalWrite(atoi(param1), atoi(param2));
-    snprintf(msg, MAX_LENGTH, "%s,$ok", _name);
+    digitalWrite(atoi(param1), atoi(param3));
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s,1", _name, param2);
   } else if (strcmp(command, "aw") == 0) {
-    analogWrite(atoi(param1), atoi(param2));
-    snprintf(msg, MAX_LENGTH, "%s,$ok", _name);
+    analogWrite(atoi(param1), atoi(param3));
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s,1", _name, param2);
   } else if (strcmp(command, "vw") == 0) {
-    Blynk.virtualWrite(atoi(param1), atoi(param2));
-    snprintf(msg, MAX_LENGTH, "%s,$ok", _name);
+    Blynk.virtualWrite(atoi(param1), atoi(param3));
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s,1", _name, param2);
   } else {
-    snprintf(msg, MAX_LENGTH, "%s,$undef", _name);
+    snprintf(msg, MAX_LENGTH, "%s,$r,%s",-1, _name, param2);
   }
   _bridge.virtualWrite(0, msg);
 }
